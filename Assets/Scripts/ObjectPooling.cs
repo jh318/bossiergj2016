7fe67faf3baa4a1116dd26df76adbaa347262929
MacMasterBlaster <mac.orchard@yahoo.com>
2016-09-10 15:38:57 -0500

    Added objectPooling

---
+++ Assets/Scripts/ObjectPooling.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ObjectPooling : MonoBehaviour {
+
+    public GameObject prefab;
+
+    private List<GameObject> objectPool = new List<GameObject>();
+
+    void Start()
+    {
+        Spawn();
+    }
+    void Spawn()
+    {
+        GameObject g = objectPool.Find(IsInactiveObject);
+
+        if (g == null)
+        {
+            g = Object.Instantiate(prefab) as GameObject;
+        }
+        else
+        {
+            g.SetActive(true);
+        }
+    }
+
+    bool IsInactiveObject(GameObject g)
+    {
+       return !g.activeSelf;
+    }
+}
