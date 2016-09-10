a3600539d39cfb03ed66f7a9e316be3cbcdd2c81
MacMasterBlaster <mac.orchard@yahoo.com>
2016-09-09 21:36:50 -0500

    Added player and enemy controllers

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,15 +2,24 @@
 using System.Collections;
 
 [RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(PolygonCollider2D))]
 public class PlayerController : MonoBehaviour {
 	
-	private RigidBody2D body;
+	public float speed = 100;
+	public float turnspeed = 5;
+	public Rigidbody2D body
+    {
+        get
+        {
+            return _body;
+        }
+    }
+    private Rigidbody2D _body;
 	private Vector2 heading;
-	public float speed = 10;
-	public float turnSpeed = 5;
+
 
 	void Start () {
-		body = gameObject.GetComponent<Rigidbody2D>();
+		_body = gameObject.GetComponent<Rigidbody2D>();
 	}
 	
 	void Update () 
@@ -18,14 +27,16 @@ public class PlayerController : MonoBehaviour {
 		float x = Input.GetAxis("Horizontal");
 		float y = Input.GetAxis("Vertical");
 		
-		Vector2 heading = new Vector2(x, y);
+		heading = new Vector2(x, y);
 
+		//stops player from drifting while no input is being added.
 		if (heading.sqrMagnitude < 0.1f)
         {
             return;
         }
         else
-        {
+        { 
+        	//turns the player object in the direction of travel.
             float angle = Mathf.Atan2(y, x) * Mathf.Rad2Deg;
             transform.rotation = Quaternion.Slerp(
                 transform.rotation,
@@ -35,9 +46,11 @@ public class PlayerController : MonoBehaviour {
 	}
 
 	void FixedUpdate(){
-        body.MovePosition(body.position + heading.normalized * speed * Time.deltaTime);
+		//this is where the players movement is actually being calculated
+        _body.velocity = (heading.normalized * speed);
 	}
 
+	// this will disable the player gameObject.
 	void OnCollisionEnter2D(Collision2D collision){
 		if (collision.gameObject.tag == "Enemy"){
 			gameObject.SetActive(false);
