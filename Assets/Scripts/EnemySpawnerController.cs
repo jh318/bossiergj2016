a30bc78f759fc266693587080241a09cb00d9835
jh318 <jhawley318@gmail.com>
2016-09-10 13:41:17 -0500

    Created basic enemy spawner pref and obj

---
+++ Assets/Scripts/EnemySpawnerController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemySpawnerController : MonoBehaviour {
+	public GameObject enemy;
+	public GameObject spawner;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if (Time.frameCount > 360.0f){
+			Instantiate(enemy);
+		}
+	
+	}
+}
