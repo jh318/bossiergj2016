a6bc3d1f090a2c0e5d09e63edb29ae3ba816ad11
Roe <yepnix@gmail.com>
2016-09-10 15:17:14 -0500

    Game Manager object, incrementing score

---
+++ Assets/Scripts/GameManagerController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManagerController : MonoBehaviour {
+
+	private FoodController[] foods;
+	private int score;
+
+	void Start () {
+		foods = FindObjectsOfType(typeof(FoodController)) as FoodController[];
+	}
+	
+	void Update () {
+		foreach (FoodController food in foods) {
+			if (food.eaten == true) {
+				score += (int)food.timeToEat;
+				Debug.Log ("Score! " + score.ToString ());
+				food.eaten = false;
+			}
+		}
+	}
+}
