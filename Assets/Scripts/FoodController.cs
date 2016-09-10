9ce701dc4ed92ef87d4e0475e8f8b34c9fe0e989
Roe <yepnix@gmail.com>
2016-09-10 13:19:10 -0500

    Food / eating mechanics

---
+++ Assets/Scripts/FoodController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FoodController : MonoBehaviour {
+
+	public float timeToEat; 
+
+	private float timeEating;
+	private Transform selfTr;
+
+	void Start () {
+		selfTr = GetComponent<Transform> ();
+		selfTr.localScale = new Vector3 (timeToEat, timeToEat);
+	}
+
+	void OnCollisionStay2D (Collision2D collision){
+		if (collision.gameObject.tag == "Player"){
+			Debug.Log (timeEating.ToString ());
+			timeEating += Time.deltaTime;
+			if (selfTr.localScale.x>=0.0f && selfTr.localScale.y>=0.0f){
+				selfTr.localScale -= new Vector3 (Time.deltaTime, Time.deltaTime);
+			}
+			if (timeEating >= timeToEat) {
+				gameObject.SetActive (false);
+			}
+		}
+	}
+}
