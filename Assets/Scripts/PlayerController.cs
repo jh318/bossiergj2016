37d2e20f248a01a898adf127bdcb641d7720fad6
jh318 <jhawley318@gmail.com>
2016-09-10 12:14:56 -0500

    pushing a360053 to top

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,52 +1,53 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(PolygonCollider2D))]
 public class PlayerController : MonoBehaviour {
-
+	
 	public float speed = 100;
 	public float turnspeed = 5;
 	public Rigidbody2D body
-	{
-		get
-		{
-			return _body;
-		}
-	}
-	private Rigidbody2D _body;
+    {
+        get
+        {
+            return _body;
+        }
+    }
+    private Rigidbody2D _body;
 	private Vector2 heading;
 
 
 	void Start () {
 		_body = gameObject.GetComponent<Rigidbody2D>();
 	}
-
+	
 	void Update () 
 	{
 		float x = Input.GetAxis("Horizontal");
 		float y = Input.GetAxis("Vertical");
-
+		
 		heading = new Vector2(x, y);
 
 		//stops player from drifting while no input is being added.
 		if (heading.sqrMagnitude < 0.1f)
-		{
-			return;
-		}
-		else
-		{ 
-			//turns the player object in the direction of travel.
-			float angle = Mathf.Atan2(y, x) * Mathf.Rad2Deg;
-			transform.rotation = Quaternion.Slerp(
-				transform.rotation,
-				Quaternion.Euler(0, 0, angle),
-				turnspeed * Time.deltaTime);
-		}  
+        {
+            return;
+        }
+        else
+        { 
+        	//turns the player object in the direction of travel.
+            float angle = Mathf.Atan2(y, x) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.Slerp(
+                transform.rotation,
+                Quaternion.Euler(0, 0, angle),
+                turnspeed * Time.deltaTime);
+        }  
 	}
 
 	void FixedUpdate(){
 		//this is where the players movement is actually being calculated
-		_body.velocity = (heading.normalized * speed);
+        _body.velocity = (heading.normalized * speed);
 	}
 
 	// this will disable the player gameObject.
