da5f1908f910e32bc47ce1dcab5526e945ea399e
MacMasterBlaster <mac.orchard@yahoo.com>
2016-09-09 20:19:36 -0500

    Adding Unity Project

---
+++ Assets/Scripts/AniminateSpritesComponent.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AniminateSpritesComponent : MonoBehaviour {
+
+    public Sprite[] animationSprites;
+    public float frameInterval = .2f;
+    public bool looping = true;
+    private SpriteRenderer _spriteRenderer;
+
+    public SpriteRenderer spriteRenderer
+    {
+        get
+        {
+            if (_spriteRenderer == null)
+            {
+                _spriteRenderer = GetComponent<SpriteRenderer>();
+            }
+            return _spriteRenderer;
+        }
+    }
+
+    void OnEnable()
+    {
+        StartCoroutine("Animate");
+    }
+
+    IEnumerator Animate()
+    {
+        if (looping)
+        {
+            while (enabled)
+            {
+                for (int i = 0; i < animationSprites.Length; i++)
+                {
+                    spriteRenderer.sprite = animationSprites[i];
+                    yield return new WaitForSeconds(frameInterval);
+                }
+            }
+        }
+        else
+        {
+            for (int i = 0; i < animationSprites.Length; i++)
+            {
+                spriteRenderer.sprite = animationSprites[i];
+                yield return new WaitForSeconds(frameInterval);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+	
+	private RigidBody2D rb;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		float x = Input.GetAxis("Horizontal");
+		float y = Input.GetAxis("Vertical");
+		
+		Vector2 heading = new Vector2(x, y);
+
+
+			
+	}
+
+	void FixedUpdate(){
+
+	}
+}
