ecdba6664e4b0bb48b6b2b5232acd77fa0b701f4
Roe <yepnix@gmail.com>
2016-09-10 21:14:35 -0500

    Some sounds (ohm-nohm)

--- Assets/Scripts/FoodController.cs
+++ Assets/Scripts/FoodController.cs
@@ -7,8 +7,10 @@ public class FoodController : MonoBehaviour {
 	[HideInInspector] public bool eaten;
 
 	private float timeEating;
+	private AudioSource nomnom;
 
 	void OnEnable () {
+		nomnom = GetComponent<AudioSource> ();
 		timeToEat = Mathf.Floor (Random.Range (1.0f, 5.0f));
 		gameObject.transform.localScale = new Vector3 (timeToEat, timeToEat);
 		eaten = false;
@@ -16,13 +18,17 @@ public class FoodController : MonoBehaviour {
 
 	void OnCollisionStay2D (Collision2D collision){
 		if (collision.gameObject.tag == "Player"){
-			Debug.Log (timeEating.ToString ());
+			nomnom.pitch += Time.deltaTime/2.0f;
+			if (!nomnom.isPlaying) {
+				nomnom.Play ();
+			}
 			timeEating += Time.deltaTime;
 			if (gameObject.transform.localScale.x>=0.1f && gameObject.transform.localScale.y>=0.1f){
 				gameObject.transform.localScale -= new Vector3 (Time.deltaTime, Time.deltaTime);
 			}
 			if (timeEating >= timeToEat) {
 				eaten = true;
+				nomnom.pitch = 1.0f;
 				GameManagerController.eatFood ();
 			}
 		}
