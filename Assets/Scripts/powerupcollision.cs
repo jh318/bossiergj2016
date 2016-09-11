66381a1eb233faa571597591faf87819fa621dea
jh318 <jhawley318@gmail.com>
2016-09-10 21:39:02 -0500

    implemented random powerup test spawn

---
+++ Assets/Scripts/powerupcollision.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class powerupcollision : MonoBehaviour {
+	
+
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+		
+	// this will disable the player gameObject.
+	void OnCollisionEnter2D(Collision2D collision){
+		if (collision.gameObject.tag == "Player"){
+			gameObject.SetActive(false);
+		}
+	}
+}
+
