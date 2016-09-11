1d80c3de3a5ad0334616e51f0eb842fc952c39bb
jh318 <jhawley318@gmail.com>
2016-09-10 21:14:24 -0500

    made enemyspawnercontroller framerate a public variable

--- Assets/Scripts/EnemySpawnerController.cs
+++ Assets/Scripts/EnemySpawnerController.cs
@@ -2,7 +2,9 @@
 using System.Collections;
 
 public class EnemySpawnerController : MonoBehaviour {
-	public GameObject enemy; //create variable to hold enemy GameObject
+	public GameObject enemy;
+	public float framerate = 360.0f; // Needs to be multiples of 6 or 60
+	//create variable to hold enemy GameObject
 	//public GameObject spawner;
 	//private Vector3 enemyPos = new Vector3(0,0,0);
 
@@ -16,7 +18,7 @@ public class EnemySpawnerController : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		
-		if (Time.frameCount%360.0f == 0){ //check if total frame count is divisiable by 360 frames IE every 6 seconds, spawn enemy
+		if (Time.frameCount%framerate == 0){ //check if total frame count is divisiable by 360 frames IE every 6 seconds, spawn enemy
 			Instantiate(enemy, GetGeneralSpawnCoordinates.enemyPos, Quaternion.identity); //create instance of object
 		}
 		//if (GetGeneralSpawnCoordinates.inArena == true) {
---
+++ Assets/Scripts/PoolMember.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;  
+using System.Collections;  
+public class PoolMember : MonoBehaviour {  
+	public Pool pool;  
+
+	void OnDisable(){  
+		pool.nextThing = gameObject;  
+	}  
+} 
