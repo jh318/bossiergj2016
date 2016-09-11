456ca2e76ccf2c67b11f404a87cf81c3e43fe509
jh318 <jhawley318@gmail.com>
2016-09-10 19:59:45 -0500

    border spawning now working

--- Assets/Scripts/BorderSpawn.cs
+++ Assets/Scripts/BorderSpawn.cs
@@ -6,25 +6,26 @@ public class BorderSpawn : MonoBehaviour {
 	//public GameObject borderLeft;
 	//public GameObject borderTop;
 	//public GameObject borderBottom;
-	public static bool inArena = false;
-	public static int xSpawn = 0;
-	public static int ySpawn = 0;
-	public static Vector3 enemyPos = new Vector3(0,0,0);
+
 	public GameObject[] borders;
 	public GameObject enemy;
+	private int b = 0;
 
 	// Use this for initialization
 	void Start () {
 		//Vector3 enemyPos = gameObject.transform.position;
 		//GameObjects[] borders;
 		borders = GameObject.FindGameObjectsWithTag("border"); //populate array with border gameobjects
+		//b = Random.Range(0, 3);
+		//Instantiate(enemy, new Vector3(borders[b].transform.position.x, borders[b].transform.position.y, 0), Quaternion.identity);
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-
+		b = Random.Range(0, 3);
 		if (Time.frameCount%360.0f == 0){ //check if total frame count is divisiable by 360 frames IE every 6 seconds, spawn enemy
-			Instantiate(enemy, borders[Random.Range(0,3)].transform.position, Quaternion.identity); //create instance of object at this position
+			Instantiate(enemy, new Vector3(borders[b].transform.position.x, borders[b].transform.position.y, 0), Quaternion.identity); //create instance of object at this position
 		}
 	
 	}
